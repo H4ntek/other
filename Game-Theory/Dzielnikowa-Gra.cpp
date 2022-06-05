935638699671e1bcfb7c7273e88750fba03c38c2
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-06-05 18:04:59 +0200

    Update Dzielnikowa-Gra.cpp

--- Game-Theory/Dzielnikowa-Gra.cpp
+++ Game-Theory/Dzielnikowa-Gra.cpp
@@ -1,4 +1,3 @@
-// zadanie z turnieju solve 4.06.2022 - DZI0
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
@@ -20,18 +19,20 @@ void solve(){
 		dp[i][1] = -INF;
 	}
 
+	// gracz 1 zbiera dodatnie punkty, a 2 ujemne
+	
     for (int i = 1; i <= n; i++){
 		dp[i][0] += a[i];
 		dp[i][1] += -a[i];
 		for (int j = 2 * i; j <= n; j += i){
-			dp[j][0] = min(dp[i][1], dp[j][0]);
-			dp[j][1] = max(dp[i][0], dp[j][1]);
+			dp[j][0] = min(dp[i][1], dp[j][0]); // g2 minimlaizuje w1 - w2, więc po tym jak g1 zrobi ruch, g2 wybierze minimum z poprzednich dp
+			dp[j][1] = max(dp[i][0], dp[j][1]); // g1 maksymalizuje w1 - w2, więc po tym jak g2 zrobi ruch, g1 wybierze maksa z poprzednich dp
 		}
 	}
 
     lli ans = dp[1][0];
     for (int i = 2; i <= n; i++){
-		ans = max(dp[i][0], ans);
+		ans = max(dp[i][0], ans); // zaczyna g1, więc do odpowiedzi bierzemy pod uwagę tylko jego starty
 	}
     cout << ans << "\n";
 }
